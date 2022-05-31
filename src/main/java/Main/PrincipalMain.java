30a15653d360eac51addcebd40e03994d49f5eae
Bradley Jacinto <bradleyivme@gmail.com>
2022-05-31 14:17:55 -0600

    Proyecto iniciado, elaboracion de ventanas

---
+++ src/main/java/Main/PrincipalMain.java
@@ -0,0 +1,11 @@
+
+package Main;
+import Inicio.Login;
+
+public class PrincipalMain {
+    public static void main(String[] args) {
+        Login is = new Login();
+        is.setVisible(true);
+        
+    }
+}
